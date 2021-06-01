52b17e4a31eb6b03ce3d9c0ab6bb6d8d21ebc419
Victor Amonde <amondev00@gmail.com>
2021-06-01 08:39:55 +0300

    Added a brackets to make x1 and x2 variables work correctly

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def calculate(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return None, None
-    x1 = (-b + math.sqrt(discriminant)) / 2 * a
-    x2 = (-b - math.sqrt(discriminant)) / 2 * a
+    x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+
+
     return x1, x2
 # calculate function finds the value of unknown in quadratic equation
 # Its takes floats and return floats
@@ -20,7 +22,6 @@ def main():
     x1, x2 = calculate(a, b, c)
     print(f"x1={x1}, x2={x2}")
     return os.EX_OK
-
 
 if __name__ == "__main__":
     sys.exit(main())
